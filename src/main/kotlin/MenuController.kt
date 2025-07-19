e2f5612a6682493e625f68cdcb3c2af7edcb2007
Alex <1965167lol@gmail.com>
2025-07-19 13:19:43 +0300

    интегрировал интерфейс в основное меню и корбку в логику меню

--- src/main/kotlin/MenuController.kt
+++ src/main/kotlin/MenuController.kt
@@ -1,16 +1,23 @@
 class MenuController(
     private val teaMenuRender: TeaMenuRender = TeaMenuRender(),
-    private val menuLogic: MenuLogic = MenuLogic(teaFactory = TeaFactory)
-) {
+    private val menuLogic: MenuLogic = MenuLogic(),
+    private val teaRepository: TeaRepository = TeaRepository
+): ChoosingTea {
     fun start() {
         while (true) {
             teaMenuRender.showMainMenu()
+            val boxWidth = printInputBox()
+            val input = readln()
+            boxBot(boxWidth)
 
-            when (readln()) {
+            when (input) {
                 "1" -> menuLogic.createTea()
                 "2" -> TODO()
                 "3" -> TODO()
-                "4" -> return
+                "4" -> {
+                    teaRepository.saveChanges()
+                    return
+                }
                 else -> println("Неверный ввод")
             }
         }
--- src/main/kotlin/MenuLogic.kt
+++ src/main/kotlin/MenuLogic.kt
@@ -1,9 +1,17 @@
-class MenuLogic(private val teaFactory: TeaFactory): ChoosingTea {
+class MenuLogic(): ChoosingTea {
 
+    private val teaFactory: TeaFactory = TeaFactory
     private val teaRepository = TeaRepository
+    private val teaTypes = TeaType.entries
 
     fun createTea() {
         chooseTeaType()
+        val boxWidth = printInputBox("Введите номер чая")
+        val teaTypeIndex = readln().toIntOrNull()
+        boxBot(boxWidth)
+
+
+
 //        teaRepository.saveTea(tea)
     }
 }
