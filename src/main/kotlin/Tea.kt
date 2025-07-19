63c06d4298a3fdb98ae070f0d795f789630fc2d2
Alex <1965167lol@gmail.com>
2025-07-19 20:40:33 +0300

    слегка изменил вывод инфы

--- src/main/kotlin/Tea.kt
+++ src/main/kotlin/Tea.kt
@@ -1,5 +1,6 @@
 import kotlinx.serialization.*
-
+private const val cyan = "\u001B[36m"
+private const val reset = "\u001B[0m"
 private const val DELIMITERS = "\n"
 
 @Serializable
@@ -17,6 +18,6 @@ data class Tea(
     }
 
     override fun toString(): String {
-        return "Id: $id$DELIMITERS Type: $type$DELIMITERS Name: $name$DELIMITERS Year: $year$DELIMITERS Rating $rating/10"
+        return "${cyan}Id: $id${DELIMITERS}Type: $type${DELIMITERS}Name: $name${DELIMITERS}Year: $year${DELIMITERS}Rating $rating/10$reset"
     }
 }
