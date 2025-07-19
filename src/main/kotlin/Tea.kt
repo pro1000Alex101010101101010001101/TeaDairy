aa046c973c9ce9a2e42b3af11e19b4630e49981e
Alex <1965167lol@gmail.com>
2025-07-19 20:07:05 +0300

    Изменил метод toString, ввел метода printInfo

--- src/main/kotlin/Tea.kt
+++ src/main/kotlin/Tea.kt
@@ -1,14 +1,22 @@
 import kotlinx.serialization.*
 
+private const val DELIMITERS = "\n"
+
 @Serializable
 data class Tea(
+    val id: Int,
     val type: TeaType,
     val name: String,
     val year: Int,
     val rating: Int,
 
 ) {
-    fun tellMeAboutTea(){
 
+    fun printInfo(){
+        println(this)
+    }
+
+    override fun toString(): String {
+        return "Id: $id$DELIMITERS Type: $type$DELIMITERS Name: $name$DELIMITERS Year: $year$DELIMITERS Rating $rating/10"
     }
 }
